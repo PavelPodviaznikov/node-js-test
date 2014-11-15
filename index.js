cf9c2e8b9e1e5a59d4a22c58800f640bd49a1fa3
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2014-11-15 15:58:15 +0200

    2

--- index.js
+++ index.js
@@ -1,65 +1,14 @@
 var express = require('express')
-var bodyParser = require('body-parser');
 var app = express();
 
-
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser());
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  var html = '<form action="/" method="post">'+
-					'a: '+
-					'<input type="text" name="a">'+
-					'<br>'+
-					'b: '+
-					'<input type="text" name="b">'+
-					'<br>'+
-					'operator: '+
-					'<input type="text" name="operator">'+
-					'<br>'+
-					'<button type="submit">Calculate</button>'+
-				'</form>';
-	response.send(html);
-});
-
-app.post('/', function (req, res) {
-	var a = req.body.a;
-	var b = req.body.b;
-	var operator = req.body.operator;
-	var html = '<form action="/" method="post">'+
-					'a: '+
-					'<input type="text" name="a">'+
-					'<br>'+
-					'b: '+
-					'<input type="text" name="b">'+
-					'<br>'+
-					'operator: '+
-					'<input type="text" name="operator">'+
-					'<br>'+
-					'<button type="submit">Calculate</button>'+
-				'</form>'+
-				'ANSWER = '+calculator(parseInt(a),parseInt(b),operator);
-	res.send(html);
-});
-
+	var answer = 2+2;
+  response.send(answer);
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
-
-function calculator(x, y, oper){
-	if(oper==="plus")
-	{
-		return x + y;
-	}
-	else if(oper==="minus"){
-		return x-y;
-	}
-	else if(oper==="multiply"){
-		return x*y;
-	}
-	else if(oper==="divide"){
-		return x/y;
-	}
-}
