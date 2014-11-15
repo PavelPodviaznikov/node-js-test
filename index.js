0b5505ff94ff0aeeb4c6a3a28bc48ada4effc1b2
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2014-11-15 16:01:27 +0200

    4

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-	var answer = 2+2;
-  response.send(answer.toString());
+	var answer = "Answer = "+(2+2);
+  response.send(answer;
 })
 
 app.listen(app.get('port'), function() {
