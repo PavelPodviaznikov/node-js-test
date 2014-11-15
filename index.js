04475959c798593760c105c76689ad24cb700a58
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2014-11-15 15:53:22 +0200

    calc2

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 var express = require('express')
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser());
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  var a=parseInt(request.param('a'));
-  var b=parseInt(request.param('b'));
-  var operator=request.param('operator');
-  var answer = calculator(a,b,operator);
-  response.send(answer||"Answer");
-})
+  var html = '<form action="/" method="post">'+
+					'a: '+
+					'<input type="text" name="a">'+
+					'<br>'+
+					'b: '+
+					'<input type="text" name="b">'+
+					'<br>'+
+					'operator: '+
+					'<input type="text" name="operator">'+
+					'<br>'+
+					'<button type="submit">Calculate</button>'+
+				'</form>';
+	response.send(html);
+});
+
+app.post('/', function (req, res) {
+	var a = req.body.a;
+	var b = req.body.b;
+	var operator = req.body.operator;
+	var html = '<form action="/" method="post">'+
+					'a: '+
+					'<input type="text" name="a">'+
+					'<br>'+
+					'b: '+
+					'<input type="text" name="b">'+
+					'<br>'+
+					'operator: '+
+					'<input type="text" name="operator">'+
+					'<br>'+
+					'<button type="submit">Calculate</button>'+
+				'</form>'+
+				'ANSWER = '+calculator(parseInt(a),parseInt(b),operator);
+	res.send(html);
+});
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
