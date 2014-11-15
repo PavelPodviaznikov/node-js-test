8c9c6177a672f1fcabdcce5a8247abe43dcb5f59
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2014-11-15 15:42:49 +0200

    calc1

--- index.js
+++ index.js
@@ -5,11 +5,30 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-	var answer = 3;
-	answer = answer.toString();
+  var a=parseInt(request.param('a'));
+  var b=parseInt(request.param('b'));
+  var operator=request.param('operator');
+  var answer = calculator(a,b,operator);
+  answer = answer.toString();
   response.send(answer);
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+function calculator(x, y, oper){
+	if(oper==="plus")
+	{
+		return x + y;
+	}
+	else if(oper==="minus"){
+		return x-y;
+	}
+	else if(oper==="multiply"){
+		return x*y;
+	}
+	else if(oper==="divide"){
+		return x/y;
+	}
+}
