bdd3a1274119b6e9db55199251da1558a682cb14
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2014-11-15 15:19:57 +0200

    answer

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Pasha!')
+	var answer = 2+2;
+  response.send(answer);
 })
 
 app.listen(app.get('port'), function() {
